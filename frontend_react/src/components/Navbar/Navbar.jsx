84c6112f05f158c15b85e5cf25b47f514d7c5e94
Gaurav <rawatgauravofficial@gmail.com>
2023-02-23 00:23:29 +0530

    added Navbar styling with Hover animation

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './Navbar.scss'
-import {images} from '../../constants'
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navabar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className="app__navbar-links">
+        {['Home', 'About', 'Contact', 'Work', 'Skills'].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#{item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
